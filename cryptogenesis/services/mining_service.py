3dd614a9322756d767ddecc3d862287d75f119fc
Max Clinton <63891960+mtclinton@users.noreply.github.com>
2025-12-31 17:25:30 -0500

    Add services directory and skeleton files (#18)

---
+++ cryptogenesis/services/mining_service.py
@@ -0,0 +1,13 @@
+"""
+Mining Service
+
+Orchestrates mining operations with state management.
+"""
+
+
+class MiningService:
+    """Service for mining operations"""
+
+    def __init__(self):
+        """Initialize mining service"""
+        pass
